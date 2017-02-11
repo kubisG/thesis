45cd2ee18b7165b924fc8b5580487efc79af603e
Jakub Gois <gois.ja89@gmail.com>
2017-02-11 22:28:16 +0100

    Thesis - very first commit. Created project structure. Added project dependencies.

---
+++ thesis-core/src/main/java/cz/osu/App.java
@@ -0,0 +1,16 @@
+package cz.osu;
+
+import javafx.event.ActionEvent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+}
