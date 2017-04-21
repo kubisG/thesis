bb1f8b8e1158ac5c2b646748776f575e98ac1b15
Jakub Gois <gois.ja89@gmail.com>
2017-04-21 14:43:48 +0200

    Thesis - minor refactoring

--- thesis-core/src/main/java/cz/osu/App.java
+++ thesis-core/src/main/java/cz/osu/App.java
@@ -1,8 +1,8 @@
 package cz.osu;
 
-import cz.osu.core.InjectedClass;
-import cz.osu.core.TestClass;
-import javafx.event.ActionEvent;
+import cz.osu.core.Composer;
+import cz.osu.core.model.Variable;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,18 +10,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+
+    public static void main( String[] args ) {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext(new String[] {"application-context.xml"});
 
-        InjectedClass injectedClass= (InjectedClass) context.getBean("injectedClass");
-        Long tmp = injectedClass.dummy();
+        Composer composer = (Composer) context.getBean("Composer");
+        Integer i = 10;
+        Variable v = new Variable("a", new Integer(20), Integer.class);
+        Class clazz = Long.class;
+        System.out.println(v.getValue().getClass());
+        System.out.println(v.getType());
+        System.out.println(clazz);
 
 
-        System.out.println( "Hello World!" + tmp);
+
+        System.out.println( "Hello World!" + composer.toString());
     }
 
 }
